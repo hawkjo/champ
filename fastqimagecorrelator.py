1aa7e65a8bc72a2768086fe372160e72a3163a13
John Hawkins <hawkjo@gmail.com>
2015-05-08 07:34:00 -0500

    Added extraction without fastq method.

--- fastqimagecorrelator.py
+++ fastqimagecorrelator.py
@@ -338,6 +338,40 @@ class FastqImageCorrelator(object):
             out.write('# Fields: ' + '\t'.join(fields) + '\n')
             out.write('\n'.join(lines))
 
+    def extract_intensity_and_sequence_from_name_list(self, name_list_fpath, out_fpath):
+        hit_given_aligned_idx = {}
+        for hit_type in ['non_mutual', 'bad_mutual', 'good_mutual', 'exclusive']:
+            for i, j in getattr(self, hit_type + '_hits'):
+                hit_given_aligned_idx[j] = (hit_type, (i, j))
+
+        hit_given_rcs_coord_tup = {(int(tile_key[-4:]), pt[0], pt[1]): hit_given_aligned_idx[i]
+                                   for i, (tile_key, pt) in enumerate(self.rcs_in_frame)}
+        rcs_coord_tups = set(hit_given_rcs_coord_tup.keys())
+
+        def flux_info_given_rcs_coord_tup(coord_tup):
+            hit_type, (i, _) = hit_given_rcs_coord_tup[coord_tup]
+            if hit_type == 'non_mutual':
+                return 'none', 0, 0
+            else:
+                sexcat_pt = self.sexcat.points[i]
+                return hit_type, sexcat_pt.flux, sexcat_pt.flux_err
+
+        lines = set()  # set rather than list due to read pairs
+        for line in open(name_list_fpath):
+            coord_tup = tuple(map(int, line.strip().split(':')[-3:]))  # tile:r:c
+            if coord_tup in rcs_coord_tups:
+                hit_type, flux, flux_err = flux_info_given_rcs_coord_tup(coord_tup)
+                lines.add('\t'.join([line.strip(),
+                                     self.image_data.fname,
+                                     hit_type,
+                                     str(flux),
+                                     str(flux_err)]))
+
+        with open(out_fpath, 'w') as out:
+            fields = ['read_name', 'image_name', 'hit_type', 'flux', 'flux_err']
+            out.write('# Fields: ' + '\t'.join(fields) + '\n')
+            out.write('\n'.join(lines))
+
     def plot_hits(self, hits, color, ax):
         for i, j in hits:
             ax.plot([self.sexcat.point_rcs[i, 1], self.aligned_rcs_in_frame[j, 1]],
