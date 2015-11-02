79198cc46698cd868216f907f64fbe7b118a3ebc
John Hawkins <hawkjo@gmail.com>
2015-11-02 15:01:26 -0600

    Removed explicit output file path.

--- read_names.sh
+++ read_names.sh
@@ -3,5 +3,4 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 miseq_info=`\ls $* | xargs gunzip -c | head -1 | awk -F ':' '{print $1 ":" $2 ":" $3}'`
-echo $miseq_info
-\ls $* | xargs gunzip -c | grep "^$miseq_info" | awk '{print $1}' | sed 's/^@//' > read_names.txt
+\ls $* | xargs gunzip -c | grep "^$miseq_info" | awk '{print $1}' | sed 's/^@//' 
