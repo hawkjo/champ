e7532c19c071487f183222e20b0a103229a1d1ed
John Hawkins <hawkjo@gmail.com>
2016-01-12 06:00:07 -0600

    Terminate if image already processed.

--- process_nd2_im.py
+++ process_nd2_im.py
@@ -74,6 +74,18 @@ def process_fig(align_run_name, nd2_fpath, align_param_fpath, im_idx):
     bname = os.path.splitext(os.path.basename(nd2_fpath))[0]
     sexcat_fpath = os.path.join(os.path.splitext(nd2_fpath)[0], '%d.cat' % im_idx)
     
+    fig_dir = os.path.join(local_config.fig_dir, align_run_name, bname)
+    results_dir = os.path.join(local_config.base_dir, 'results', align_run_name, bname)
+    for d in [fig_dir, results_dir]:
+        if not os.path.exists(d):
+            os.makedirs(d)
+    all_read_rcs_fpath = os.path.join(results_dir, '{}_all_read_rcs.txt'.format(im_idx))
+
+    if os.path.isfile(all_read_rcs_fpath):
+        print bname, im_idx, ' already done.'
+
+    intensity_fpath = os.path.join(results_dir, '{}_intensities.txt'.format(im_idx))
+    stats_fpath = os.path.join(results_dir, '{}_stats.txt'.format(im_idx))
     fic = fastqimagecorrelator.FastqImageCorrelator(project_name)
     tile_data=local_config.fastq_tiles_given_read_name_fpath(aligning_read_names_fpath)
     fic.load_reads(tile_data)
@@ -82,14 +94,6 @@ def process_fig(align_run_name, nd2_fpath, align_param_fpath, im_idx):
     fic.align(possible_tile_keys, rotation_est, fq_w_est, snr_thresh=snr_thresh, min_hits=min_hits, hit_type=['exclusive', 'good_mutual'])
     print project_name, bname, im_idx, ','.join(tile.key for tile in fic.hitting_tiles)
     
-    fig_dir = os.path.join(local_config.fig_dir, align_run_name, bname)
-    results_dir = os.path.join(local_config.base_dir, 'results', align_run_name, bname)
-    for d in [fig_dir, results_dir]:
-        if not os.path.exists(d):
-            os.makedirs(d)
-
-    intensity_fpath = os.path.join(results_dir, '{}_intensities.txt'.format(im_idx))
-    stats_fpath = os.path.join(results_dir, '{}_stats.txt'.format(im_idx))
     fic.output_intensity_results(intensity_fpath)
     fic.write_alignment_stats(stats_fpath)
 
@@ -102,7 +106,6 @@ def process_fig(align_run_name, nd2_fpath, align_param_fpath, im_idx):
     all_fic = fastqimagecorrelator.FastqImageCorrelator(project_name)
     tile_data = local_config.fastq_tiles_given_read_name_fpath(all_read_names_fpath)
     all_fic.all_reads_fic_from_aligned_fic(fic, tile_data)
-    all_read_rcs_fpath = os.path.join(results_dir, '{}_all_read_rcs.txt'.format(im_idx))
     all_fic.write_read_names_rcs(all_read_rcs_fpath)
 
 if __name__ == '__main__':
