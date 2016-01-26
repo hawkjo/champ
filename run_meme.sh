1cd93eedd8eab9169255f3d348095a0d8fa5e5b4
John Hawkins <hawkjo@gmail.com>
2016-01-26 11:49:44 -0600

    Removed legacy files.

--- run_meme.sh
+++
@@ -1,9 +0,0 @@
-if [ $# -ne 1 ]; then
-    echo "Usage: run_meme.sh <fpath>"
-    exit 1;
-fi
-
-f=$1
-out_dir="meme_results_`echo $f | sed 's/.fa*//'`"
-
-meme $f -dna -o $out_dir -nostatus -time 18000 -maxsize 60000 -mod anr -nmotifs 3 -minw 6 -maxw 50 -revcomp
