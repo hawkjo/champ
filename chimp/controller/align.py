18e6958972c281699837685bf2e23fbce17cddc2
Jim Rybarski <jim@rybarski.com>
2016-04-12 18:51:37 -0500

    moved everything

--- chimp/controller/align.py
+++ chimp/controller/align.py
@@ -1,5 +1,9 @@
-from chimp import align as al
+from chimp.config import AlignmentParameters, Experiment
+from error import quit
+from chimp.process_nd2_im import process_fig
 
 
-def main(arguments):
-    al.main('15-11-18_SA15243_Cascade-TA_1nM-007', alignment_offset=1)
+def align(clargs):
+    alignment_parameters = AlignmentParameters(clargs)
+    file_structure = Experiment()
+    process_fig(alignment_parameters, 'fast', clargs.)
