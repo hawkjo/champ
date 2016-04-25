006d1b12317d1c4873c0c7ea05a9bbb8ebfec978
John Hawkins <hawkjo@gmail.com>
2016-04-25 18:31:48 -0500

    Overhaul to correctly collate channels

--- sum_tifs_to_hdf5.py
+++ sum_tifs_to_hdf5.py
@@ -5,13 +5,13 @@ import os
 import glob
 import re
 import numpy as np
+from collections import defaultdict
 
 
-def tif_dir_to_hdf5(Major_axis_idx, channel, hdf5_fpath, tif_fpaths):
+def tif_dir_to_hdf5(Major_axis_idx, hdf5_fpath, tif_fpaths):
     Major_axis_group = Major_axis_idx + 1
     minor_axis_group = (1 - Major_axis_idx) + 1
     with h5py.File(hdf5_fpath, 'a') as f:
-        g = f.create_group(channel)
         for fpath in tif_fpaths:
             sys.stdout.write('.')
             sys.stdout.flush()
@@ -21,31 +21,51 @@ def tif_dir_to_hdf5(Major_axis_idx, channel, hdf5_fpath, tif_fpaths):
             Major_axis_pos = int(m.group(Major_axis_group))
             minor_axis_pos = int(m.group(minor_axis_group))
 
-            im = tifffile.imread(fpath)
-            if not (len(im.shape) == 3 and im.shape[1] == im.shape[2]):
-                print '\nWarning: Skipping im_shape {} in {}'.format(im.shape, fpath)
-                continue
-            out_im = im.sum(axis=0)
-            assert out_im.shape == im.shape[1:], out_im.shape
+            with tifffile.TiffFile(fpath) as tif:
+                summary = tif.micromanager_metadata['summary']
 
-            dset_name = '(Major, minor) = ({}, {})'.format(Major_axis_pos, minor_axis_pos)
-            dset = g.create_dataset(dset_name, out_im.shape, dtype=out_im.dtype)
-            dset [...] = out_im
+                # Find channel names and assert unique 
+                channel_names = [name.replace(' ', '_').replace('(', '').replace(')', '')
+                                 for name in summary['ChNames']]
+                assert summary['Channels'] == len(channel_names) == len(set(channel_names)), channel_names
+
+                # channel_idxs map tif pages to channels
+                channel_idxs = tif.micromanager_metadata['index_map']['channel']
+
+                # Setup defaultdict
+                h, w = summary['Height'], summary['Width']
+                def hw_zeros():
+                    return np.zeros((h, w), dtype=np.int)
+                summed_images = defaultdict(hw_zeros)
+
+                # Add images
+                for channel_idx, page in zip(channel_idxs, tif.pages):
+                    summed_images[channel_idx] += page.asarray()
+
+                # Add images to hdf5
+                dset_name = '(Major, minor) = ({}, {})'.format(Major_axis_pos, minor_axis_pos)
+                for idx, channel_name in enumerate(channel_names):
+                    if channel_name not in f:
+                        g = f.create_group(channel_name)
+                    else:
+                        g = f[channel_name]
+
+                    out_im = summed_images[idx]
+                    dset = g.create_dataset(dset_name, out_im.shape, dtype=out_im.dtype)
+                    dset[...] = out_im
     print
             
 
 if __name__ == '__main__':
-    usg_fmt = '{} <Major_axis_idx(0,1)> <channel> <hdf5_fpath> <tif_fpaths>'.format(sys.argv[0])
+    usg_fmt = '{} <Major_axis_idx(0,1)> <hdf5_fpath> <tif_fpaths>'.format(sys.argv[0])
     if len(sys.argv) < len(usg_fmt.split()):
         sys.exit(usg_fmt)
 
     Major_axis_idx = int(sys.argv[1])
     assert Major_axis_idx in [0, 1], 'Major axis must be in (0, 1)'
 
-    channel = sys.argv[2]
+    hdf5_fpath = sys.argv[2]
 
-    hdf5_fpath = sys.argv[3]
+    tif_fpaths = sys.argv[3:]
 
-    tif_fpaths = sys.argv[4:]
-
-    tif_dir_to_hdf5(Major_axis_idx, channel, hdf5_fpath, tif_fpaths)
+    tif_dir_to_hdf5(Major_axis_idx, hdf5_fpath, tif_fpaths)
