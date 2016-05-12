c353d8d312cf63b79469fc86b7f3c808328d7bb8
Jim Rybarski <jim@rybarski.com>
2016-05-12 13:34:53 -0500

    safer and easier conversion

--- chimp/controller/convert.py
+++ chimp/controller/convert.py
@@ -1,11 +1,19 @@
-from chimp.convert import tif_dir_to_hdf5
+from chimp import convert
+import logging
 import os
+
+log = logging.getLogger(__name__)
 
 
 def main(clargs):
     for directory in clargs.tif_directories:
+        if not os.path.isdir(directory):
+            log.debug("Skipping non-directory %s" % directory)
+            continue
         tif_filenames = [f for f in os.listdir(directory) if f.endswith(".ome.tif")]
-        print(tif_filenames)
         if tif_filenames:
             hdf5_file_path = "%s.h5" % directory
-            tif_dir_to_hdf5(hdf5_file_path, tif_filenames, clargs.flipud, clargs.fliplr)
+            log.debug("About to convert %d files in %s to HDF5 file: %s" % (len(tif_filenames),
+                                                                            directory,
+                                                                            hdf5_file_path))
+            convert.tif_dir_to_hdf5(hdf5_file_path, tif_filenames, clargs.flipud, clargs.fliplr)
