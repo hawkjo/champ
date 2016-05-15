835cbc4f9dd4c657a44cfa37fdaf34485b8e1d3f
Jim Rybarski <jim@rybarski.com>
2016-05-15 18:20:06 -0500

    remove log message

--- chimp/controller/align.py
+++ chimp/controller/align.py
@@ -12,7 +12,6 @@ log = logging.getLogger(__name__)
 def main(clargs):
     h5_filenames = list(filter(lambda x: x.endswith('.h5'), os.listdir(clargs.image_directory)))
     h5_filenames = [os.path.join(clargs.image_directory, filename) for filename in h5_filenames]
-    log.debug("h5filenames: %s" % h5_filenames)
     experiment = Experiment(clargs.project_name)
     um_per_pixel = 0.26666666
     alignment_parameters = AlignmentParameters(clargs)
