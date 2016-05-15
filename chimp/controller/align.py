4333dfa95825d81c3c86e2a77454a7eeb7aa7bee
Jim Rybarski <jim@rybarski.com>
2016-05-15 17:51:56 -0500

    fixed h5 filename bug

--- chimp/controller/align.py
+++ chimp/controller/align.py
@@ -11,12 +11,15 @@ log = logging.getLogger(__name__)
 
 def main(clargs):
     h5_filenames = list(filter(lambda x: x.endswith('.h5'), os.listdir(clargs.image_directory)))
+    h5_filenames = [os.path.join(clargs.image_directory, filename) for filename in h5_filenames]
+    log.debug("h5filenames: %s" % h5_filenames)
     experiment = Experiment(clargs.project_name)
     um_per_pixel = 0.26666666
     alignment_parameters = AlignmentParameters(clargs)
+    log.debug("Loading tile data.")
     all_tile_data = align.load_read_names(alignment_parameters.all_read_names_filepath)
     phix_tile_data = align.load_read_names(alignment_parameters.aligning_read_names_filepath)
-
+    log.debug("Tile data loaded.")
     processes = min(len(h5_filenames), multiprocessing.cpu_count())
     log.debug("Using %d processes for alignment" % processes)
     pool = multiprocessing.Pool(processes=processes)
