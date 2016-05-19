177a58a575fb548d94c81775542b5f993fc7d92e
Jim Rybarski <jim@rybarski.com>
2016-05-19 17:51:41 -0500

    added some comments for clarity

--- chimp/align.py
+++ chimp/align.py
@@ -15,47 +15,58 @@ log = logging.getLogger(__name__)
 
 
 def run(h5_filenames, alignment_parameters, alignment_tile_data, experiment, um_per_pixel, channel):
+    # Each process will work on a single concentration and find which Illumina tiles we have data for
+    # To combine the results into a single dictionary we use a multiprocessing Manager dictionary
     end_tiles = Manager().dict()
+
+    # We need a method to wrap all the static arguments, since map_async() takes just one function
+    # and a single iterable as arguments
     boundary_finder = functools.partial(find_boundary_columns, channel, alignment_parameters,
                                         alignment_tile_data, um_per_pixel, experiment, end_tiles)
+
+    # We use one process per concentration. We could theoretically speed this up since our machine
+    # has significantly more cores than the typical number of concentration points, but since it
+    # usually finds a result in the first image or two, it's not going to deliver any practical benefits
     num_processes = len(h5_filenames)
     pool = multiprocessing.Pool(num_processes)
     log.debug("Finding boundaries with %d processes" % num_processes)
     pool.map_async(boundary_finder, h5_filenames).get(timeout=sys.maxint)
-    if not end_tiles:
-        log.debug("whoops, no end tiles")
-        exit()
     log.debug("Done finding boundaries!")
-    exit()
+
     # Iterate over images that are probably inside an Illumina tile, attempt to align them, and if they
     # align, do a precision alignment and write the mapped FastQ reads to disk
     num_processes = multiprocessing.cpu_count()
     log.debug("Aligning all images with %d cores" % num_processes)
-    alignment_func = functools.partial(alignment, alignment_parameters, um_per_pixel,
+    alignment_func = functools.partial(perform_alignment, alignment_parameters, um_per_pixel,
                                        experiment, alignment_tile_data)
     pool = multiprocessing.Pool(num_processes)
-    pool.map_async(alignment_func, iterate_all_images(h5_filenames, end_tiles, channel)).get(timeout=sys.maxint)
+    pool.map_async(alignment_func,
+                   iterate_all_images(h5_filenames, end_tiles, channel)).get(timeout=sys.maxint)
     log.debug("Done aligning!")
 
 
-def alignment(alignment_parameters, um_per_pixel, experiment, alignment_tile_data, image_data):
-        image, possible_tile_keys, base_name = image_data
-        log.debug("Aligning image from %s. Row: %d, Column: %d " % (base_name, image.row, image.column))
-        # first get the correlation to random tiles, so we can distinguish signal from noise
-        fia = process_fig(alignment_parameters, base_name, alignment_tile_data,  um_per_pixel,
-                          experiment, image, possible_tile_keys)
-        if fia.hitting_tiles:
-            # The image data aligned with FastQ reads!
-            fia.precision_align_only(hit_type=('exclusive', 'good_mutual'),
-                                     min_hits=alignment_parameters.min_hits)
-            write_output(image.index, base_name, fia, experiment, alignment_tile_data)
-        # The garbage collector takes its sweet time for some reason, so we have to manually delete
-        # these objects or memory usage blow up
-        del fia
-        del image
+def perform_alignment(alignment_parameters, um_per_pixel, experiment, alignment_tile_data, image_data):
+    # Does a rough alignment, and if that works, does a precision alignment and writes the corrected
+    # FastQ reads to disk
+    image, possible_tile_keys, base_name = image_data
+    log.debug("Aligning image from %s. Row: %d, Column: %d " % (base_name, image.row, image.column))
+    # first get the correlation to random tiles, so we can distinguish signal from noise
+    fia = process_fig(alignment_parameters, base_name, alignment_tile_data,  um_per_pixel,
+                      experiment, image, possible_tile_keys)
+    if fia.hitting_tiles:
+        # The image data aligned with FastQ reads!
+        fia.precision_align_only(hit_type=('exclusive', 'good_mutual'),
+                                 min_hits=alignment_parameters.min_hits)
+        write_output(image.index, base_name, fia, experiment, alignment_tile_data)
+    # The garbage collector takes its sweet time for some reason, so we have to manually delete
+    # these objects or memory usage blows up
+    del fia
+    del image
 
 
 def iterate_all_images(h5_filenames, end_tiles, channel):
+    # To allow full parallel processing of data, we need to be able to iterate over every image
+    # The order doesn't really matter since alignment can be done independently
     for h5_filename in h5_filenames:
         base_name = os.path.splitext(h5_filename)[0]
         with h5py.File(h5_filename) as h5:
@@ -79,12 +90,12 @@ def find_boundary_columns(channel, alignment_parameters, alignment_tile_data, um
         # Find the outermost columns of image data where we overlap with FastQ tile reads
         # We do this so we can skip any images that are definitely not going to be useful to us
         left_column, right_column, tile_map = find_ends(grid, figure_processor)
-        print("!!!", base_name, left_column, right_column, tile_map)
         end_tiles[base_name] = left_column, right_column, tile_map
-        print("endtiles", end_tiles)
 
 
 def load_read_names(file_path):
+    # reads a FastQ file with Illumina read names
+    # TODO: Does this add the read name and sequence to the value?
     with open(file_path) as f:
         tiles = defaultdict(set)
         for line in f:
@@ -116,22 +127,32 @@ def find_ends(grid, figure_processor):
 
 def get_expected_tile_map(left_tiles, right_tiles, min_column, max_column):
     # Creates a dictionary that relates each column of microscope images to its expected tile, +/- 1.
+    # Works regardless of whether everything is flipped upsidedown (i.e. the lower tile is on the
+    # right side)
     tile_map = defaultdict(list)
+
+    # We gets lists of tiles, so we have to work out the minimum and maximum number in a slightly
+    # complicated way
     left_tiles = [int(tile.key[-4:]) for tile in left_tiles]
     right_tiles = [int(tile.key[-4:]) for tile in right_tiles]
     min_tile = min(itertools.chain(left_tiles, right_tiles))
     max_tile = max(itertools.chain(left_tiles, right_tiles))
-    if min_tile not in left_tiles:
-        reverse = True
-    else:
-        reverse = False
+
+    # Keep track of whether we'll have to invert all the associations of tiles and columns
+    invert_map = True if min_tile not in left_tiles else False
+
+    # Find the "tiles per column" factor so we can map a column to a tile
     normalization_factor = float(abs(max_tile - min_tile) + 1) / float(max_column - min_column)
+
+    # Build up the map
     for column in range(min_column, max_column + 1):
         expected = int(round(normalization_factor * column)) - 1
         expected = min(constants.MISEQ_TILE_COUNT, max(0, expected)) + min_tile
-        tile_map_column = column if not reverse else max_column - column
+        tile_map_column = column if not invert_map else max_column - column
+        # We definitely need to check the predicted tile
         tile_map[tile_map_column].append(format_tile_number(expected))
-        # print(expected, min_tile, max_tile, reverse)
+        # If we're at a boundary, we just want to check the adjacent tile towards the middle
+        # If we're in the middle, we want to check the tiles on either side
         if expected < max_tile:
             tile_map[tile_map_column].append(format_tile_number(expected + 1))
         if expected > min_tile:
