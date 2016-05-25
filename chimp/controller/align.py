b08afb2b33a73992aab7f8043efada22e277a5f0
Jim Rybarski <jim@rybarski.com>
2016-05-25 16:26:02 -0500

    hack

--- chimp/controller/align.py
+++ chimp/controller/align.py
@@ -17,16 +17,16 @@ def main(clargs):
     um_per_pixel = 0.2666666666
     alignment_parameters = AlignmentParameters(clargs)
     log.debug("Loading tile data.")
-    # phix_tile_data = align.load_read_names(alignment_parameters.aligning_read_names_filepath)
-    all_tile_data = align.load_read_names(alignment_parameters.all_read_names_filepath)
-    # print(all_tile_data)
-    # all_tile_data.update(phix_tile_data)
-    # print(sum([len(val) for val in phix_tile_data.values()]))
-    # print(sum([len(val) for val in all_tile_data.values()]))
-    exit()
-    # log.debug("Tile data loaded.")
-    # align.run(h5_filenames, alignment_parameters, phix_tile_data, all_tile_data, experiment,
-    #           um_per_pixel, clargs.alignment_channel)
+    phix_tile_data = align.load_read_names(alignment_parameters.aligning_read_names_filepath)
+    print(sum([len(v) for v in phix_tile_data.values()]))
+    unclassified_tile_data = align.load_read_names(alignment_parameters.all_read_names_filepath)
+    print(sum([len(v) for v in unclassified_tile_data.values()]))
+    all_tile_data = {key: phix_tile_data.get(key, []) + unclassified_tile_data.get(key, [])
+                     for key in list(unclassified_tile_data.keys()) + list(phix_tile_data.keys())}
+    print(sum([len(v) for v in all_tile_data.values()]))
+    log.debug("Tile data loaded.")
+    align.run(h5_filenames, alignment_parameters, phix_tile_data, all_tile_data, experiment,
+              um_per_pixel, clargs.alignment_channel)
 
 
 # def second(clargs):
