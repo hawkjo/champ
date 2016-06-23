38bf06ec286672a19e623fec799c11fc1b169a4d
Jim Rybarski <jim@rybarski.com>
2016-06-23 14:03:35 -0500

    debug commands

--- chimp/main.py
+++ chimp/main.py
@@ -37,7 +37,7 @@ def main(**kwargs):
     log = logging.getLogger()
     log.addHandler(logging.StreamHandler())
     log.setLevel(arguments.log_level)
-    log.debug(str(arguments))
+    log.debug(str(arguments.__dict__))
 
     # make some space to distinguish log messages from command prompt
     for _ in range(2):
