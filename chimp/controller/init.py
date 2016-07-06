4f8fcd475310c56d67f54946b16849725dd27ce3
Jim Rybarski <jim@rybarski.com>
2016-07-06 15:01:14 -0500

    init fully functional now

--- chimp/controller/init.py
+++ chimp/controller/init.py
@@ -1,4 +1,4 @@
-from chimp import convert
+from chimp import convert, fits
 import logging
 
 log = logging.getLogger(__name__)
@@ -8,4 +8,8 @@ def main(clargs):
     paths = convert.get_all_tif_paths(clargs.image_directory)
     # directories will have ".h5" appended to them to come up with the HDF5 names
     # tifs are relative paths to each tif file
+    log.debug("About to convert TIFs to HDF5.")
     convert.main(paths, clargs.flipud, clargs.fliplr)
+    log.debug("Done converting TIFs to HDF5.")
+    log.debug("Fitsifying images from HDF5 files.")
+    fits.main(clargs.image_directory)
