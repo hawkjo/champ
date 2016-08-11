70220715251fec15bf36a44122b4f52ad9572636
Jim Rybarski <jim@rybarski.com>
2016-08-11 12:25:26 -0500

    updated debug statements

--- champ/align.py
+++ champ/align.py
@@ -264,7 +264,7 @@ def write_output(image_index, base_name, fastq_image_aligner, output_parameters,
         return
 
     # save information about how to align the images
-    log.info("Saving alignment with score of %s" % new_stats.score)
+    log.info("Saving alignment with score of %s\t\t%s" % (new_stats.score, base_name))
     with open(stats_file_path, 'w') as f:
         yaml.dump(new_stats, f)
 
