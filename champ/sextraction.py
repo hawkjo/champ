4d1876dc41537adee57d6f6542995120b502b4d9
Jim Rybarski <jim@rybarski.com>
2016-09-14 11:42:31 -0500

    #9 replace Source Extractor and astropy.io.fits with our own implementation of a cluster finding algorithm

--- champ/sextraction.py
+++ champ/sextraction.py
@@ -2,21 +2,16 @@ import numpy as np
 
 
 class SextractorPoint(object):
+    __slots__ = ('r', 'c')
+
     def __init__(self, line):
-        self.c, self.r, self.flux, self.flux_err, self.flags, \
-                self.width, self.height, self.theta \
-                = map(float, line.strip().split())
-        # Sextractor coordinates are 1-based
-        self.r -= 1
-        self.c -= 1
+        self.c, self.r = map(float, line.strip().split())
 
 
 class Sextraction(object):
     def __init__(self, lines):
         self.points = []
         for line in lines:
-            if line.startswith('#'):
-                continue
             self.points.append(SextractorPoint(line))
         self.point_rcs = np.array([(pt.r, pt.c) for pt in self.points])
 
