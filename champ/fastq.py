44d893a4ef6e5159cc2e30fdb3c589ed8a8421f0
Jim Rybarski <jim@rybarski.com>
2016-10-11 15:54:58 -0500

    moved two classes out of obsolete file to consolidate read mapping functionality

--- champ/fastq.py
+++ champ/fastq.py
@@ -1,92 +1,10 @@
 import gzip
 import logging
 import os
-import pysam
-import subprocess
+
 
 log = logging.getLogger(__name__)
 
-
-class FastqFiles(object):
-    """ Sorts compressed FastQ files provided to us from the Illumina sequencer. """
-    def __init__(self, filenames):
-        self._filenames = list(self._filter_names(filenames))
-
-    def __iter__(self):
-        for f in self._filenames:
-            yield f
-
-    def __len__(self):
-        return len(self._filenames)
-
-    @property
-    def alignment_length(self):
-        paired_length = len([(f1, f2) for f1, f2 in self.paired])
-        single_length = len([f for f in self.single])
-        return paired_length + single_length
-
-    @property
-    def paired(self):
-        for f1, f2 in self._sort_filenames(paired=True):
-            yield f1, f2
-
-    @property
-    def single(self):
-        for f in self._sort_filenames(paired=False):
-            yield f
-
-    def _filter_names(self, data):
-        # eliminate filenames that can't possibly be fastq files of interest
-        for filename in reversed(data):
-            if not filename.endswith('fastq.gz'):
-                continue
-            if '_I1_' in filename or '_I2_' in filename or '_I1.' in filename or '_I2.' in filename:
-                continue
-            yield filename
-
-    def _sort_filenames(self, paired=True):
-        # yield filenames that are the given type (single or paired)
-        for filename in self._filenames:
-            if '_R1_' in filename or '_R1.' in filename:
-                pair = filename.replace('_R1_', '_R2_').replace('_R1.', '_R2.')
-                if paired and pair in self._filenames:
-                    yield filename, pair
-                elif not paired and pair not in self._filenames:
-                    yield filename
-
-
-class FastqReadClassifier(object):
-    def __init__(self, bowtie_path):
-        clean_path = bowtie_path.rstrip(os.path.sep)
-        self.name = os.path.basename(clean_path)
-        self._common_command = ('bowtie2', '--local', '-p 15', '--no-unal', '-x %s' % clean_path)
-
-    def paired_call(self, fastq_file_1, fastq_file_2):
-        command = self._common_command + ('-1 ' + fastq_file_1,
-                                          '-2 ' + fastq_file_2,
-                                          '-S chimp.sam',
-                                          '2>&1 | tee error.txt')
-        return self._run(command)
-
-    def single_call(self, fastq_file):
-        command = self._common_command + ('-U ' + fastq_file,)
-        return self._run(command)
-
-    def _run(self, command):
-        with open('/dev/null', 'w+') as devnull:
-            shell_options = dict(shell=True, stderr=devnull, stdout=devnull)
-            subprocess.call(' '.join(command), **shell_options)
-            sam_command = 'samtools view -bS chimp.sam | samtools sort - final'
-            subprocess.call(sam_command, **shell_options)
-            subprocess.call('samtools index final.bam', **shell_options)
-            for r in pysam.Samfile('final.bam'):
-                yield r.qname
-        for temp_file in ('chimp.sam', 'final.bam', 'error.txt', 'final.bam.bai'):
-            try:
-                os.unlink(temp_file)
-            except (OSError, IOError):
-                log.warn("Unable to delete temp file: %s. "
-                         "Was it not created? You may be missing FASTQ reads." % temp_file)
 
 
 def classify_fastq_reads(classifier, fastq_files):
--- champ/readmap.py
+++ champ/readmap.py
@@ -1,5 +1,4 @@
 from Bio import SeqIO
-from champ import fastq
 from champ.adapters_cython import simple_hamming_distance
 from collections import defaultdict
 import editdistance
@@ -9,7 +8,9 @@ import logging
 import numpy as np
 import os
 import pickle
+import pysam
 import random
+import subprocess
 import yaml
 
 log = logging.getLogger(__name__)
@@ -23,7 +24,7 @@ def main(clargs):
 
     """
     fastq_filenames = [os.path.join(clargs.fastq_directory, directory) for directory in os.listdir(clargs.fastq_directory)]
-    fastq_files = fastq.FastqFiles(fastq_filenames)
+    fastq_files = FastqFiles(fastq_filenames)
     read_names_given_seq = {}
 
     if clargs.log_p_file_path:
@@ -72,8 +73,91 @@ def main(clargs):
     write_all_read_names(read_names_given_seq, os.path.join(clargs.output_directory, 'all_read_names.txt'))
 
 
+
+class FastqFiles(object):
+    """ Sorts compressed FastQ files provided to us from the Illumina sequencer. """
+    def __init__(self, filenames):
+        self._filenames = list(self._filter_names(filenames))
+
+    def __iter__(self):
+        for f in self._filenames:
+            yield f
+
+    def __len__(self):
+        return len(self._filenames)
+
+    @property
+    def alignment_length(self):
+        paired_length = len([(f1, f2) for f1, f2 in self.paired])
+        single_length = len([f for f in self.single])
+        return paired_length + single_length
+
+    @property
+    def paired(self):
+        for f1, f2 in self._sort_filenames(paired=True):
+            yield f1, f2
+
+    @property
+    def single(self):
+        for f in self._sort_filenames(paired=False):
+            yield f
+
+    def _filter_names(self, data):
+        # eliminate filenames that can't possibly be fastq files of interest
+        for filename in reversed(data):
+            if not filename.endswith('fastq.gz'):
+                continue
+            if '_I1_' in filename or '_I2_' in filename or '_I1.' in filename or '_I2.' in filename:
+                continue
+            yield filename
+
+    def _sort_filenames(self, paired=True):
+        # yield filenames that are the given type (single or paired)
+        for filename in self._filenames:
+            if '_R1_' in filename or '_R1.' in filename:
+                pair = filename.replace('_R1_', '_R2_').replace('_R1.', '_R2.')
+                if paired and pair in self._filenames:
+                    yield filename, pair
+                elif not paired and pair not in self._filenames:
+                    yield filename
+
+
+class FastqReadClassifier(object):
+    def __init__(self, bowtie_path):
+        clean_path = bowtie_path.rstrip(os.path.sep)
+        self.name = os.path.basename(clean_path)
+        self._common_command = ('bowtie2', '--local', '-p 15', '--no-unal', '-x %s' % clean_path)
+
+    def paired_call(self, fastq_file_1, fastq_file_2):
+        command = self._common_command + ('-1 ' + fastq_file_1,
+                                          '-2 ' + fastq_file_2,
+                                          '-S chimp.sam',
+                                          '2>&1 | tee error.txt')
+        return self._run(command)
+
+    def single_call(self, fastq_file):
+        command = self._common_command + ('-U ' + fastq_file,)
+        return self._run(command)
+
+    def _run(self, command):
+        with open('/dev/null', 'w+') as devnull:
+            shell_options = dict(shell=True, stderr=devnull, stdout=devnull)
+            subprocess.call(' '.join(command), **shell_options)
+            sam_command = 'samtools view -bS chimp.sam | samtools sort - final'
+            subprocess.call(sam_command, **shell_options)
+            subprocess.call('samtools index final.bam', **shell_options)
+            for r in pysam.Samfile('final.bam'):
+                yield r.qname
+        for temp_file in ('chimp.sam', 'final.bam', 'error.txt', 'final.bam.bai'):
+            try:
+                os.unlink(temp_file)
+            except (OSError, IOError):
+                log.warn("Unable to delete temp file: %s. "
+                         "Was it not created? You may be missing FASTQ reads." % temp_file)
+
+
 def find_reads_using_bamfile(bamfile_path, fastq_files):
-    classifier = fastq.FastqReadClassifier(bamfile_path)
+    classifier = FastqReadClassifier(bamfile_path)
     read_names = set()
     for file1, file2 in fastq_files.paired:
         for read in classifier.paired_call(file1, file2):
