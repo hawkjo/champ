c4ffd42fdb6a31b3ec31931a03d9ef70523c3cff
Jim Rybarski <jim@rybarski.com>
2016-12-12 22:26:45 -0600

    #27 save mutable data in a separate cache file so that the metadata file stays unchanged and easy to inspect for errors

--- champ/initialize.py
+++ champ/initialize.py
@@ -4,8 +4,11 @@ from champ.error import fail
 
 
 def save(clargs):
-    filename = os.path.join(clargs.image_directory, 'champ.yml')
-    with open(filename, 'w+') as f:
+    metadata_filename = os.path.join(clargs.image_directory, 'champ.yml')
+    cache_filename = os.path.join(clargs.image_directory, 'cache.yml')
+
+    # save static data that should never change unless the user makes a mistake
+    with open(metadata_filename, 'w') as f:
         data = {'chip_name': clargs.chip_name,
                 'mapped_reads': os.path.abspath(clargs.mapped_reads),
                 'parsed_reads': os.path.abspath(clargs.parsed_reads),
@@ -19,33 +22,53 @@ def save(clargs):
                 'alternate_good_target_reads_filename': clargs.alternate_good_target_reads_filename,
                 'flipud': clargs.flipud,
                 'fliplr': clargs.fliplr,
-                'perfect_target_name': clargs.perfect_target_name,
-                'phix_aligned': False,
+                'perfect_target_name': clargs.perfect_target_name
+                }
+        yaml.dump(data, f)
+
+    # save empty cache
+    with open(cache_filename, 'w') as f:
+        data = {'phix_aligned': False,
                 'preprocessed': False,
                 'protein_channels_aligned': []}
         yaml.dump(data, f)
 
 
-def update(image_directory, metadata):
-    filename = get_existing_metadata_filename(image_directory)
-    with open(filename, 'w+') as f:
-        yaml.dump(metadata, f)
+def update_cache(image_directory, cache):
+    filename = os.path.join(image_directory, 'cache.yml')
+    with open(filename, 'w') as f:
+        yaml.dump(cache, f)
 
 
-def load(image_directory):
-    filename = get_existing_metadata_filename(image_directory)
+def load_cache_and_metadata(image_directory):
+    metadata_filename = get_existing_metadata_filename(image_directory)
+    cache_filename = os.path.join(image_directory, 'cache.yml')
     try:
-        with open(filename) as fh:
-            return yaml.load(fh)
+        with open(metadata_filename) as fh:
+            metadata = yaml.load(fh)
     except IOError:
         fail("The image directory you provided (%s) has not been initialized. We need you to provide metadata with"
              "the 'champ init' command first." % str(image_directory))
     except:
         fail("Something is wrong with the metadata file in the image directory. Try rerunning 'champ init'.", 2)
+    else:
+        try:
+            with open(cache_filename) as fh:
+                cache = yaml.load(fh)
+        except IOError:
+            # make separation of cache file backwards compatible with datasets that didn't start with it
+            cache = {}
+            cache['phix_aligned'] = metadata['phix_aligned']
+            cache['preprocessed'] = metadata['preprocessed']
+            cache['protein_channels_aligned'] = metadata['protein_channels_aligned']
+            if 'end_tiles' in metadata:
+                cache['end_tiles'] = metadata['end_tiles']
+    return metadata, cache
 
 
 def get_existing_metadata_filename(image_directory):
-    # Look for the metadata file, and if it doesn't exist, try again with the old extension to allow backwards compatibility
+    # Look for the metadata file, and if it doesn't exist,
+    # try again with the old extension to allow backwards compatibility
     filename = os.path.join(image_directory, 'champ.yml')
     if not os.path.exists(filename):
         filename = os.path.join(image_directory, 'champ.yaml')
