0e56dcd3d3e7d7dbc9bdb7670f249de8a96dce2a
Jim Rybarski <jim@rybarski.com>
2017-04-23 10:03:06 -0500

    document function

--- champ/error.py
+++ champ/error.py
@@ -1,4 +1,4 @@
 def fail(message, return_code=1):
-    # Instead of dumping stack traces back the user we provide them a readable message and a return code
+    # Instead of dumping stack traces back the user we provide them a readable message and a composable return code
     print(message)
     exit(return_code)
