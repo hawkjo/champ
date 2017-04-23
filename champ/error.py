b551ed13771b797e63573e5c20cca3b497fa0adf
Jim Rybarski <jim@rybarski.com>
2017-04-23 10:02:08 -0500

    bump version number

--- champ/error.py
+++ champ/error.py
@@ -1,3 +1,4 @@
 def fail(message, return_code=1):
+    # Instead of dumping stack traces back the user we provide them a readable message and a return code
     print(message)
     exit(return_code)
