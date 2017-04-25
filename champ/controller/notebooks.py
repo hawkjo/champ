06a476e228124cbf357928def5d9c61485cf8893
Jim Rybarski <jim@rybarski.com>
2017-04-25 11:14:29 -0500

    Made warnings display regardless of log level

--- champ/controller/notebooks.py
+++ champ/controller/notebooks.py
@@ -8,13 +8,14 @@ log = logging.getLogger(__name__)
 
 def main(clargs):
     current_directory = os.getcwd()
-
     base = os.path.join(os.path.abspath(os.path.join(os.path.dirname(champ.__file__), '..')), 'notebooks')
     notebooks = os.listdir(base)
+
     for notebook in notebooks:
         notebook_path = os.path.join(base, notebook)
         destination = os.path.join(current_directory, notebook)
-        if os.path.isfile(destination):
-            log.warn("{notebook} already exists! SKIPPING!".format(notebook=notebook))
+        if os.path.exists(destination):
+            print("{notebook} already exists! SKIPPING!".format(notebook=notebook))
             continue
         shutil.copy(notebook_path, destination)
+        print("Created {notebook}".format(notebook=notebook))
