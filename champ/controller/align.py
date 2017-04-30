d4241c5a0a499b78578f13d2730d8a09021c22e1
Jim Rybarski <jim@rybarski.com>
2017-04-29 20:56:46 -0500

    special one-off branch to help convert some weird files

--- champ/controller/align.py
+++ champ/controller/align.py
@@ -10,11 +10,11 @@ cluster_strategies = ('se', 'otsu',)
 
 def preprocess(clargs, metadata, cache):
     log.debug("Preprocessing images.")
-    paths = convert.get_all_tif_paths(clargs.image_directory)
+    # paths = convert.get_all_tif_paths(clargs.image_directory)
     # directories will have ".h5" appended to them to come up with the HDF5 names
     # tifs are relative paths to each tif file
     log.debug("About to convert TIFs to HDF5.")
-    convert.main(paths, metadata['flipud'], metadata['fliplr'])
+    # convert.main(paths, metadata['flipud'], metadata['fliplr'])
     log.debug("Done converting TIFs to HDF5.")
     log.debug("Fitsifying images from HDF5 files.")
     fits.main(clargs.image_directory)
