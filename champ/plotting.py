8adf43085913a35ae693ec37f56fd8af859303cc
Jim Rybarski <jim@rybarski.com>
2017-06-02 18:03:29 -0500

    sequence-independent diff plot

--- champ/plotting.py
+++ champ/plotting.py
@@ -260,31 +260,25 @@ def plot_2d_mismatches(sequence, sequence_labels, lower_ABA_matrix, base_colors=
     cbar.set_label('$ABA (k_{B}T)$', fontsize=fontsize*2)
 
 
-def plot_mismatch_diff(sequence, sequence_labels, lower_ABA_matrix, base_colors=None, upper_ABA_matrix=None, fontsize=18):
-    if base_colors is None:
-        base_colors = {'A': flabpal.blue, 'C': flabpal.yellow, 'G': flabpal.green, 'T': flabpal.red}
-    dimension = 3
-    width_ratios = [.5, 1, len(sequence) * dimension, 3]
-    height_ratios = [len(sequence) * dimension, 1, .5]
-    fig = plt.figure(figsize=(sum(width_ratios) / 3, sum(height_ratios) / 3))
+def plot_diff(sequence, sequence_labels, lower_ABA_matrix, upper_ABA_matrix=None, fontsize=18):
+    width_ratios = [.5, len(sequence), 1]
+    height_ratios = [len(sequence), .5]
+    fig = plt.figure(figsize=(sum(width_ratios), sum(height_ratios)))
 
-    gs = gridspec.GridSpec(3, 4,
+    gs = gridspec.GridSpec(2, 3,
                            width_ratios=width_ratios,
                            height_ratios=height_ratios,
-                           wspace=0.01, hspace=0.01
-                           )
-    data_index = 2
+                           wspace=0.01, hspace=0.01)
+    data_index = 1
     left_sequence_index = 0
-    bottom_sequence_index = 10
-    left_color_index = 1
-    bottom_color_index = 6
-    cbar_index = 3
+    bottom_sequence_index = 4
+    cbar_index = 2
 
     # Add the sequence labels to the left of the figure
     left_sequence_ax = fig.add_subplot(gs[left_sequence_index])
-    left_sequence_ax.set_yticklabels(sequence_labels[::-1], fontsize=8*dimension)
-    left_sequence_ax.set_yticks([dimension * x + dimension / 2.0 for x in range(len(sequence))])
-    left_sequence_ax.set_ylim([0, len(sequence) * dimension])
+    left_sequence_ax.set_yticklabels(sequence_labels[::-1], fontsize=fontsize)
+    left_sequence_ax.set_yticks([x + 0.5 for x in range(len(sequence))])
+    left_sequence_ax.set_ylim([0, len(sequence)])
     left_sequence_ax.spines['top'].set_visible(False)
     left_sequence_ax.spines['right'].set_visible(False)
     left_sequence_ax.spines['bottom'].set_visible(False)
@@ -297,9 +291,9 @@ def plot_mismatch_diff(sequence, sequence_labels, lower_ABA_matrix, base_colors=
 
     # Add the sequence labels to the bottom of the figure
     bottom_sequence_ax = fig.add_subplot(gs[bottom_sequence_index])
-    bottom_sequence_ax.set_xticklabels(sequence_labels, fontsize=8*dimension)
-    bottom_sequence_ax.set_xticks([dimension * x + 1.5 for x in range(len(sequence))])
-    bottom_sequence_ax.set_xlim([0, len(sequence) * dimension - 1])
+    bottom_sequence_ax.set_xticklabels(sequence_labels, fontsize=fontsize)
+    bottom_sequence_ax.set_xticks([x + 0.5 for x in range(len(sequence))])
+    bottom_sequence_ax.set_xlim([0, len(sequence)])
     bottom_sequence_ax.spines['top'].set_visible(False)
     bottom_sequence_ax.spines['right'].set_visible(False)
     bottom_sequence_ax.spines['bottom'].set_visible(False)
@@ -310,13 +304,6 @@ def plot_mismatch_diff(sequence, sequence_labels, lower_ABA_matrix, base_colors=
     bottom_sequence_ax.tick_params(left="off")
     bottom_sequence_ax.set_yticklabels([])
 
-    # Add the color bars to the left and bottom of the figure to indicate which base the mismatch has been converted to
-    mm_bases = ''.join(['ACGT'.replace(base, '') for base in sequence])
-    left_color_codes_ax = fig.add_subplot(gs[left_color_index])
-    build_base_colorcode_axis(left_color_codes_ax, mm_bases, base_colors, vertical=True)
-    bottom_color_codes_ax = fig.add_subplot(gs[bottom_color_index])
-    build_base_colorcode_axis(bottom_color_codes_ax, mm_bases, base_colors)
-
     # Add data to the main part of the figure
     data_ax = fig.add_subplot(gs[data_index])
     data_ax.set_axis_bgcolor(0.87 * np.array([1, 1, 1]))
@@ -325,7 +312,7 @@ def plot_mismatch_diff(sequence, sequence_labels, lower_ABA_matrix, base_colors=
     else:
         # we "add" the arrays, retaining NaNs, to create a comparison matrix
         # both matrices should have their include_diagonal_values=False or else those will sum,
-        # or if one is on it will be misleading
+        # and if one is on it will be misleading
         ms = data_ax.matshow(sum_nan_arrays(upper_ABA_matrix, lower_ABA_matrix), cmap='RdBu')
     data_ax.set_yticks([])
     data_ax.set_xticks([])
@@ -334,8 +321,7 @@ def plot_mismatch_diff(sequence, sequence_labels, lower_ABA_matrix, base_colors=
     cbar_ax = fig.add_subplot(gs[cbar_index])
     cbar_ax.tick_params(labelsize=18)
     cbar = plt.colorbar(ms, cax=cbar_ax)
-    cbar.set_clim(-1.0, 1.0)
-    cbar.set_label('$ABA (k_{B}T)$', fontsize=fontsize*2)
+    cbar.set_label('$\Delta\ ABA (k_{B}T)$', fontsize=fontsize * 2)
 
 
 def plot_2d_deletions(sequence, sequence_labels, lower_ABA_matrix, upper_ABA_matrix=None, fontsize=18):
