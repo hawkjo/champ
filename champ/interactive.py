f42bfd91abe0bf71140534e2d664e124930e0824
Jim Rybarski <jim@rybarski.com>
2017-06-04 16:42:59 -0500

    handle None value correctly

--- champ/interactive.py
+++ champ/interactive.py
@@ -153,8 +153,9 @@ class TwoDMatrix(object):
                 else:
                     c = column
                     r = row
-                value = np.mean(values)
-                if normalize_by is not None:
+                clean_values = tuple(v for v in values if v is not None)
+                value = np.mean(clean_values) if clean_values else None
+                if normalize_by is not None and value is not None:
                     value /= normalize_by
                 if (side == 'lower' and not flip_sequence) or (side == 'upper' and flip_sequence):
                     data[r, c] = value
