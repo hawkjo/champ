7982f6600db516899325b23c812eb111f6d405b3
Jim Rybarski <jim@rybarski.com>
2017-06-07 16:58:56 -0500

    remove incorrect len

--- champ/plotting.py
+++ champ/plotting.py
@@ -111,7 +111,7 @@ def sum_nan_arrays(a, b):
     mb = np.isnan(b)
     summed = np.where(ma & mb, np.nan, np.where(ma, 0, a) + np.where(mb, 0, b))
     # Null out any diagonal values since they no longer hold any meaning, or at best will be deceptive
-    for i in range(len(summed.shape[0])):
+    for i in range(summed.shape[0]):
         summed[i, i] = np.nan
 
 
