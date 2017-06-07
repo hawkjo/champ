a55607f22b1446a4b36527039d97fdeff23735f2
Jim Rybarski <jim@rybarski.com>
2017-06-07 17:04:42 -0500

    remove diagonals when two matrices are displayed

--- champ/plotting.py
+++ champ/plotting.py
@@ -111,13 +111,9 @@ def sum_nan_arrays(a, b):
     mb = np.isnan(b)
     summed = np.where(ma & mb, np.nan, np.where(ma, 0, a) + np.where(mb, 0, b))
     # Null out any diagonal values since they no longer hold any meaning, or at best will be deceptive
-    print("summed.shape", summed.shape)
-    print("one shape", summed.shape[0])
-    print("type summed", type(summed))
     for i in range(summed.shape[0]):
-        print("i", i)
-        print(summed[i,i])
-        summed[i, i] = 0.0
+        summed[i, i] = np.nan
+    return summed
 
 
 def get_gridspec(sequence, dimension):
