1c6990adba79e1931cec65f590d1439ed886a675
Jim Rybarski <jim@rybarski.com>
2017-10-12 12:46:01 -0500

    remove restriction that is enforced elsewhere

--- champ/kd_genome.py
+++ champ/kd_genome.py
@@ -35,6 +35,10 @@ class GenomicSequence(object):
         if self._upstream is not None and self._downstream is not None:
             inferred_start = self.fasta_start + len(self._upstream)
             inferred_end = self.fasta_start + self.isize - len(self._downstream)
+            print(self._upstream)
+            print(fasta[self.reference_id][self.fasta_start:self.fasta_start + self.isize])
+            print(self._downstream)
+            print()
             return ("%s%s%s" % (self._upstream, fasta[self.reference_id][inferred_start:inferred_end], self._downstream)).upper()
         return None
 
