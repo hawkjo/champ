13c4408822c9b289e96a4fb565f58a80f6679875
Jim Rybarski <jim@rybarski.com>
2018-01-24 14:18:04 -0600

    gave up on just sending image directly, object attributes are being deleted somehow

--- champ/align.py
+++ champ/align.py
@@ -294,8 +294,17 @@ def iterate_all_images(h5_filenames, end_tiles, channel):
             for column in range(min_column, max_column):
                 for row in range(grid._height):
                     image = grid.get(row, column)
-                    if image is not None:
-                        yield row, column, channel, h5_filename, tile_map[image.column], base_name
+                    if image is None:
+                        continue
+                    stats_path = os.path.join(path_info.results_directory, base_name,
+                                              '{}_stats.txt'.format(image.index))
+                    alignment_path = os.path.join(path_info.results_directory, base_name,
+                                                  '{}_all_read_rcs.txt'.format(image.index))
+                    already_aligned = alignment_is_complete(stats_path) and os.path.exists(alignment_path)
+                    if already_aligned:
+                        log.debug("Image already aligned/checkpointed: {}/{}".format(h5_filename, image.index))
+                        continue
+                    yield row, column, channel, h5_filename, tile_map[image.column], base_name
 
 
 def load_read_names(file_path):
