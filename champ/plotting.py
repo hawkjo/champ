96bc2413668867af538d409f0076df14a73fdf86
Jim Rybarski <jim@rybarski.com>
2018-03-20 21:51:40 -0500

    capitalize D

--- champ/plotting.py
+++ champ/plotting.py
@@ -376,7 +376,7 @@ def get_cluster_counts(ia, seq):
 def configure_position_penalty_axes(target, fig, penalty_axes, xticklabels, fontsize, tick_fontsize,
                                     yaxis_type, target_name, legend=True, count_axes=None):
     if yaxis_type == 'kd':
-        yaxis_label = '$K_{d} (nM)$'
+        yaxis_label = '$K_{D} (nM)$'
         penalty_axes.set_yscale('log')
     elif yaxis_type == 'ddG':
         yaxis_label = '$\Delta \Delta G\ (K_{B}T)$'
