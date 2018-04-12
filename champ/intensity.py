b19b02967d9695b4529b747e3ba25cf0aacb3af2
Jim Rybarski <jim@rybarski.com>
2018-04-11 23:35:47 -0500

    limit processors temporarily

--- champ/intensity.py
+++ champ/intensity.py
@@ -354,6 +354,7 @@ def determine_cluster_intensities(lda_weights_fpath, h5_fpaths, results_dirs, pi
         with progressbar.ProgressBar() as pbar:
             for channel, major, minor, read_name, score in pbar(lomp.parallel_iterator(work_items,
                                                                                        _thread_calculate_raw_cluster_intensities,
-                                                                                       args=(pixel_radius, lda_weights))):
+                                                                                       args=(pixel_radius, lda_weights),
+                                                                                       process_count=8)):
                 scores[h5_fpath][channel][(major, minor)][read_name] = score
     return scores
