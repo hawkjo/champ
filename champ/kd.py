258d0537a5a6af58b8624290c33d0fad090badc7
Jim Rybarski <jim@rybarski.com>
2018-04-12 18:00:15 -0500

    change fitting function

--- champ/kd.py
+++ champ/kd.py
@@ -6,7 +6,9 @@ import matplotlib.pyplot as plt
 import misc
 import itertools
 from champ import seqtools
-from biofits import fit_hyperbola
+# from biofits import fit_hyperbola
+from scipy import stats
+from biofits import hyperbola
 
 
 BOOTSTRAP_ROUNDS = 20
@@ -531,6 +533,38 @@ class IAKdData(object):
         if Kd is None:
             return None
         return self.log_neg_control_Kd - np.log(Kd)
+
+
+def fit_hyperbola(concentrations, signals):
+    """
+    :param concentrations: X-axis values representing concentrations in arbitrary units
+    :param signals: Y-axis values representing some kind of signal. Don't normalize this.
+
+    :return:
+        yint: the Y-intercept of the fit, often the background signal
+        yint_stddev: standard deviation of the error of yint
+        delta_y: total height of the fit
+        delta_y_stddev: standard deviation of the error of delta_y
+        kd: the dissociation constant
+        kd_stddev: the standard deviation of the error of kd
+
+    """
+    signals = [np.mean(s) for s in signals]
+    sigmas = [np.std(s) for s in signals]
+    if 0 in sigmas:
+        # if we have scalar values, the standard deviation will be zero everywhere and this will cause a division by
+        # zero to occur. In this case, we just weight all points equally
+        sigmas = np.ones((len(signals),))
+    (yint, delta_y, kd), covariance = curve_fit(hyperbola,
+                                                concentrations,
+                                                signals,
+                                                bounds=((-np.inf, 0.0, 10**-100),
+                                                        (np.inf, np.inf, np.inf)),
+                                                sigma=sigmas)
+    yint_stddev = covariance[0, 0] ** 0.5
+    delta_y_stddev = covariance[1, 1] ** 0.5
+    kd_stddev = covariance[2, 2] ** 0.5
+    return yint, yint_stddev, delta_y, delta_y_stddev, kd, kd_stddev
 
 
 def fit_kd(all_intensities, all_concentrations):
