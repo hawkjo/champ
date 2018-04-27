7f28d7cc304a69dc850f90988d50eeade3d53244
Jim Rybarski <jim@rybarski.com>
2018-04-27 17:13:57 -0500

    limit process count

--- champ/gbtools.py
+++ champ/gbtools.py
@@ -101,7 +101,8 @@ def calculate_genomic_kds(bamfile, read_name_intensities_hdf5_filename, concentr
     with progressbar.ProgressBar(max_value=len(pileup_data)) as pbar:
         for contig, position, result in pbar(lomp.parallel_map(pileup_data,
                                                                determine_kd_of_genomic_position,
-                                                               args=(read_name_intensities, concentrations, delta_y))):
+                                                               args=(read_name_intensities, concentrations, delta_y),
+                                                               process_count=8)):
             if result is not None:
                 kd, kd_uncertainty, yint, fit_delta_y, count = result
                 contig_position_kds[contig][position] = kd, kd_uncertainty, count
