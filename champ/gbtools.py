31fdb5d49cf5e8dd9e0ccec5fd2f14b6d1513e2d
Jim Rybarski <jim@rybarski.com>
2018-04-29 00:02:45 -0500

    fix progressbar

--- champ/gbtools.py
+++ champ/gbtools.py
@@ -195,10 +195,10 @@ def calculate_genomic_kds(bamfile, read_name_intensities_hdf5_filename, concentr
             pileup_data = []
             for result in iterate_pileups(bamfile, contig):
                 pileup_data.append(result)
-            for contig, position, result in pbar(lomp.parallel_map(pileup_data,
-                                                                   determine_kd_of_genomic_position,
-                                                                   args=(read_name_intensities, concentrations, delta_y),
-                                                                   process_count=process_count)):
+            for contig, position, result in lomp.parallel_map(pileup_data,
+                                                              determine_kd_of_genomic_position,
+                                                              args=(read_name_intensities, concentrations, delta_y),
+                                                              process_count=process_count):
                 if result is not None:
                     kd, kd_uncertainty, yint, fit_delta_y, count = result
                     contig_position_kds[contig][position] = kd, count
