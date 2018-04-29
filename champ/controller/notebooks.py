11cbe9bfe48d0a0c9c5ffe66cf21948e8980a007
Jim Rybarski <jim@rybarski.com>
2018-04-29 14:14:47 -0500

    debug notebooks

--- champ/controller/notebooks.py
+++ champ/controller/notebooks.py
@@ -1,13 +1,18 @@
+import logging
 import os
 import champ
 import shutil
 
+log = logging.getLogger(__name__)
+
 
 def main(clargs):
     current_directory = os.getcwd()
+    log.debug("Current directory: {current}".format(current=current_directory))
     base = os.path.join(os.path.abspath(os.path.join(os.path.dirname(champ.__file__), '..')), 'notebooks')
+    log.debug("Base notebook directory: {base}".format(base=base))
     notebooks = os.listdir(base)
-
+    log.debug("Notebooks: {notebooks}".format(notebooks=" ".join(notebooks)))
     for notebook in notebooks:
         notebook_path = os.path.join(base, notebook)
         destination = os.path.join(current_directory, notebook)
