f164c14afe2f4933caa17b7cf090b570b3568627
Jim Rybarski <jim@rybarski.com>
2019-01-16 10:59:06 -0600

    lower threshold for data and abandon kd fitting

--- notebooks/intensity-kon-fitting.py
+++ notebooks/intensity-kon-fitting.py
@@ -8,7 +8,6 @@ import numpy as np
 import yaml
 
 from champ import misc, intensity, initialize, seqtools, interactive
-from champ.kd import calculate_all_synthetic_kds
 from champ.seqtools import build_interesting_sequences
 
 
@@ -142,12 +141,13 @@ int_scores.get_LDA_scores(results_dirs, nonneg_lda_weights_fpath)
 int_scores.normalize_scores()
 int_scores.print_reads_per_channel()
 
-# The number of observations we require to consider a cluster valid enough for fitting.
-# Clusters at the edge of a field of view might not be visible in every concentration due to
-# random imperfections in the motion of the stage, and some fields of view might simply not
-# align under each concentration.
-minimum_observations_per_cluster = len(h5_fpaths) - 3
-print("minimum_observations_per_cluster", minimum_observations_per_cluster)
+
+# It used to be important for fitting to have multiple observations of each cluster. However, with the new on rate
+# determination scheme, we only need two consecutive observations at the right point to get meaningful data. We
+# definitely can't use clusters with 0 or 1 observations, so we just throw those out to save time. If the observations
+# we do have aren't at the right titration, we'll throw the cluster out later, so we don't need to worry about making
+# sure the cluster is actually usable at this point.
+minimum_observations_per_cluster = 2
 
 int_scores.build_good_read_names(minimum_observations_per_cluster)
 good_read_names = int_scores.good_read_names
@@ -188,10 +188,3 @@ with h5py.File(read_name_kd_filename, 'a') as h5:
     h5.create_dataset('intensities', data=intensity_matrix)
 
 all_concentrations = [misc.parse_concentration(h5_fpath) for h5_fpath in h5_fpaths]
-
-calculate_all_synthetic_kds(read_name_kd_filename,
-                            all_concentrations,
-                            interesting_read_names,
-                            target,
-                            neg_control_target,
-                            process_count)
